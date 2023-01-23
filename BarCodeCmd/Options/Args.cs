6a0431855d44635d6ef8f163a5c6c7f1141a9fa3
Vitalii Shapran <vitalii.shapran@fadv.com>
2023-01-23 14:34:57 -0500

    Implemented v1.0

---
+++ BarCodeCmd/Options/Args.cs
@@ -0,0 +1,18 @@
+﻿using CommandLine;
+
+namespace BarCodeCmd.Options;
+
+internal sealed class Args
+{
+    [Option('i', "input", Required = true)]
+    public string? Input { get; set; }
+
+    [Option('w', "width", Required = true)]
+    public int Width { get; set; }
+
+    [Option('h', "height", Required = true)]
+    public int Height { get; set; }
+
+    [Option('d', "dest", Required = true)]
+    public string? Destination { get; set; }
+}
---
+++ BarCodeCmd/Program.cs
@@ -0,0 +1,29 @@
+﻿using BarcodeLib;
+using System.Drawing.Imaging;
+using System.Drawing;
+using BarCodeCmd.Options;
+using CommandLine;
+
+Parser.Default.ParseArguments<Args>(args).WithParsed(a =>
+{
+    var foreColor = Color.Black;
+    var backColor = Color.White;
+
+    using var barcode = new Barcode();
+
+    var img = barcode.Encode(TYPE.CODE128, a.Input, foreColor, backColor,
+        a.Width, a.Height);
+
+    if (string.IsNullOrEmpty(a.Destination))
+    {
+        throw new ArgumentException("Destination cannot be empty. Please specify valid png file path");
+    }
+
+    var outputDir = Path.GetDirectoryName(a.Destination);
+    if (outputDir != null && !Directory.Exists(outputDir))
+    {
+        Directory.CreateDirectory(outputDir);
+    }
+
+    img.Save(a.Destination, ImageFormat.Png);
+});
