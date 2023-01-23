bf7ca8b184917fc913a589548424821d777b2e6a
Vitalii Shapran <vitalii.shapran@fadv.com>
2023-01-23 14:49:01 -0500

    Improved help info

--- BarCodeCmd/Options/Args.cs
+++ BarCodeCmd/Options/Args.cs
@@ -4,15 +4,15 @@ namespace BarCodeCmd.Options;
 
 internal sealed class Args
 {
-    [Option('i', "input", Required = true)]
+    [Option('i', "input", Required = true, HelpText = "Input string")]
     public string? Input { get; set; }
 
-    [Option('w', "width", Required = true)]
+    [Option('w', "width", Required = true, HelpText = "Width of the target image")]
     public int Width { get; set; }
 
-    [Option('h', "height", Required = true)]
+    [Option('h', "height", Required = true, HelpText = "Height of the target image")]
     public int Height { get; set; }
 
-    [Option('d', "dest", Required = true)]
+    [Option('d', "dest", Required = true, HelpText = "Destination image file. Should have .png extension")]
     public string? Destination { get; set; }
 }
